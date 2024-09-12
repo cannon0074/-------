eff249849999a0c0e038e94c9d24fcaa8fe94ce5
Sanmoondee11 <sanmoon2001fu@gmail.com>
2024-09-13 00:23:35 +0900

    ヘッダー　css追加

--- pj-team-reazemi/src/components/Header.jsx
+++ pj-team-reazemi/src/components/Header.jsx
@@ -1,7 +1,8 @@
 import React from "react";
+import "./Header.css";
 
 const Header = () => {
-  return <h1>ZP Books</h1>;
+  return <h1 className="header">ZP Books</h1>;
 };
 
 export default Header;
