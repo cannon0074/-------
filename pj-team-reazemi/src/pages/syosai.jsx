57246f4548a64a406d6254fd2b6b501c18abd171
Nana <nam.th.ia.1127@gmail.com>
2024-09-19 12:35:07 +0900

    ヘッダー組み込み

--- pj-team-reazemi/src/pages/syosai.jsx
+++ pj-team-reazemi/src/pages/syosai.jsx
@@ -3,6 +3,8 @@ import ReactLogo from "../assets/react.svg";
 import CartButton from "../components/CartButton";
 import FavoliteButton from "../components/FavoliteButton";
 import styled from "styled-components";
+import Header from "../components/Header";
+import UserInfo from "../components/UserInfo";
 
 // `p`タグをベースにしたスタイル付きコンポーネント
 const BookName = styled.p`
@@ -18,28 +20,32 @@ const ButtonGroup = styled.div`
 
 const Syosai = () => {
   return (
-    <div>
-      <h2>■ 商品詳細</h2>
-      <img src={ReactLogo} alt="" />
-
+    <>
+      <Header />
+      <UserInfo />
       <div>
-        <div>【書籍名】</div>
-        <BookName>
-          モダンJavaScriptの基本から始めるReact実践の教科書(最新ReactHooks対応)(Informatics&IDEA)
-        </BookName>
-        <div>【概要】</div>
-        <p>
-          【令和最新版】Reactのすべてがこの一冊で網羅できる完全版！これを持たずにReactは語れない！
-        </p>
-        <div>【価格】</div>
-        <p>￥2,860（税込）</p>
-      </div>
+        <h2>■ 商品詳細</h2>
+        <img src={ReactLogo} alt="" />
 
-      <ButtonGroup>
-        <CartButton></CartButton>
-        <FavoliteButton></FavoliteButton>
-      </ButtonGroup>
-    </div>
+        <div>
+          <div>【書籍名】</div>
+          <BookName>
+            モダンJavaScriptの基本から始めるReact実践の教科書(最新ReactHooks対応)(Informatics&IDEA)
+          </BookName>
+          <div>【概要】</div>
+          <p>
+            【令和最新版】Reactのすべてがこの一冊で網羅できる完全版！これを持たずにReactは語れない！
+          </p>
+          <div>【価格】</div>
+          <p>￥2,860（税込）</p>
+        </div>
+
+        <ButtonGroup>
+          <CartButton></CartButton>
+          <FavoliteButton></FavoliteButton>
+        </ButtonGroup>
+      </div>
+    </>
   );
 };
 
